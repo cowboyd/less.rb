3b4a41e991a9e0b7086c46cb64b7d4ec8d6d5c34
kares <self@kares.org>
2014-03-23 16:13:05 +0100

    2104051e42f0 did not fix Rhino/JRuby compatibility with less 1.6.x

--- lib/less/java_script/rhino_context.rb
+++ lib/less/java_script/rhino_context.rb
@@ -13,11 +13,11 @@ end
 module Less
   module JavaScript
     class RhinoContext
-      
+
       def self.instance
         return new # NOTE: for Rhino a context should be kept open per thread !
       end
-      
+
       def initialize(globals = nil)
         @rhino_context = Rhino::Context.new :java => true
         if @rhino_context.respond_to?(:version)
@@ -32,7 +32,7 @@ module Less
       def unwrap
         @rhino_context
       end
-      
+
       def exec(&block)
         @rhino_context.open(&block)
       rescue Rhino::JSError => e
@@ -41,7 +41,7 @@ module Less
 
       def eval(source, options = {})
         source = source.encode('UTF-8') if source.respond_to?(:encode)
-        
+
         source_name = options[:source_name] || "<eval>"
         line_number = options[:line_number] || 1
         @rhino_context.eval("(#{source})", source_name, line_number)
@@ -51,14 +51,14 @@ module Less
 
       def call(properties, *args)
         options = args.last.is_a?(::Hash) ? args.pop : {} # extract_option!
-        
+
         source_name = options[:source_name] || "<eval>"
         line_number = options[:line_number] || 1
         @rhino_context.eval(properties, source_name, line_number).call(*args)
       rescue Rhino::JSError => e
         handle_js_error(e)
       end
-      
+
       def method_missing(symbol, *args)
         if @rhino_context.respond_to?(symbol)
           @rhino_context.send(symbol, *args)
@@ -66,35 +66,35 @@ module Less
           super
         end
       end
-      
+
       private
-      
+
         def handle_js_error(e)
           if e.value && ( e.value['message'] || e.value['type'].is_a?(String) )
             raise Less::ParseError.new(e, e.value) # LessError
           end
-          if e.unwrap.to_s =~ /missing closing `\}`/
+          if e.unwrap.to_s =~ /missing opening `\(`/
             raise Less::ParseError.new(e.unwrap.to_s)
           end
           if e.message && e.message[0, 12] == "Syntax Error"
             raise Less::ParseError.new(e)
           else
             raise Less::Error.new(e)
-          end          
+          end
         end
-        
+
         def apply_1_8_compatibility!
           # TODO rather load ecma-5.js ...
           @rhino_context.eval("
             // String
-            if ( ! String.prototype.trim ) {  
-              String.prototype.trim = function () {  
-                return this.replace(/^\s+|\s+$/g,'');  
-              };  
+            if ( ! String.prototype.trim ) {
+              String.prototype.trim = function () {
+                return this.replace(/^\s+|\s+$/g,'');
+              };
             }
           ")
         end
-        
+
     end
   end
 end
